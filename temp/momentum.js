4ddf27acf5e73e0cd4bc22f8ce40f6dbfe52fd09
lsg001008 <lsg001008@gmail.com>
2022-03-15 12:01:20 +0900

    :hammer: Connect to local storage

--- temp/momentum.js
+++ temp/momentum.js
@@ -1,6 +1,16 @@
+/**
+ * html 요소를 선택해서 변수에 할당
+ *
+ * 1. document 전체에서 "id=login" 태그를 찾아 loginForm에 저장
+ * 2. loginForm의 자식 요소 중에서 <input> 태그를 찾아 loginInput에 저장
+ */
 const loginForm = document.querySelector("#login");
+const loginInput = loginForm.querySelector("input");
 
-// 로그인 폼이 제출될 때 수행할 함수
+// 반복되는 문자열(String)은 오타 방지를 위해 상수로 설정
+const USERNAME_KEY = "username";
+
+// 로그인 폼이 제출(submit)될 때 수행할 함수
 const onLoginSubmit = function (event) {
   /**
    * preventDefault() : 기본 동작 방지
@@ -9,6 +19,12 @@ const onLoginSubmit = function (event) {
    * 로그인 버튼 눌러도 아무 동작도 하지 않음
    */
   event.preventDefault();
+
+  // loginInput에 입력된 value를 username에 저장
+  const username = loginInput.value;
+
+  // 로컬 스토리지에 Key:Value 쌍을 저장
+  localStorage.setItem(USERNAME_KEY, username);
 };
 
 /**
