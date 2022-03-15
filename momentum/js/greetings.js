d8eb0904a25bb2ff3c3ed3a7b7b1d2ec701a7f3b
lsg001008 <lsg001008@gmail.com>
2022-03-15 16:41:05 +0900

    :construction: Work in progress

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,11 +1,11 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = document.querySelector('#login-form input');
-const greeting = document.querySelector('#greeting');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = 'hidden';
-const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-const link = document.querySelector('a');
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -24,7 +24,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener('submit', onLoginSubmit);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
