fd3d4f7443fb12775d894f667aa3ae1df3fefc3e
lsg001008 <lsg001008@gmail.com>
2022-04-06 17:44:00 +0900

    :alembic: Perform experiment

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import Card from './Component';
+import Modal from './Modal';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Card
-        title='Fast Performance'
-        description='Get your blood tests delivered at home collect a sample from the news your blood tests.'
-      />
-
-      <Card
-        title='Prototyping'
-        description='Get your blood tests delivered at home collect a sample from the news your blood tests.'
-      />
-
-      <Card
-        title='Vector Editing'
-        description='Get your blood tests delivered at home collect a sample from the news your blood tests.'
-      />
+      <Modal></Modal>
     </>
   );
 }
---
+++ begin-react/src/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Modal() {
+  return (
+    <>
+      <div className='modalWrapper'>
+        <p>이날의 일정이 없습니다.</p>
+      </div>
+    </>
+  );
+}
+
+export default Modal;
