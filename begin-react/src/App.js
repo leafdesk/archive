844fad48be98ce5ff621d04bc6f80713eb0d05e8
lsg001008 <lsg001008@gmail.com>
2022-04-11 17:18:15 +0900

    :construction: Work in progress

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import Wrapper from './02_Wrapper/Wrapper';
-import Counter from './03_Counter/Counter';
 
 function App() {
-  return (
-    <Wrapper>
-      <Counter></Counter>
-    </Wrapper>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
