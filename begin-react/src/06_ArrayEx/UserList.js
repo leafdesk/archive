607316438e7dec7c5a21d9cbd68395bf76aa4a7e
lsg001008 <lsg001008@gmail.com>
2022-04-18 16:03:41 +0900

    :hammer: Update UserList.js

--- begin-react/src/06_ArrayEx/UserList.js
+++ begin-react/src/06_ArrayEx/UserList.js
@@ -2,16 +2,26 @@ import React from 'react';
 
 // velopert (public.velopert@gmail.com) 등. 한 명의 유저 컴포넌트
 // UserList Component 에서 user 속성을 붙인 <User /> 컴포넌트를 만든다.
-const User = ({ user, onRemove }) => {
+const User = ({ user, onRemove, onToggle }) => {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black',
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 };
 
-const UserList = ({ users, onRemove }) => {
+const UserList = ({ users, onRemove, onToggle }) => {
   return (
     <div>
       {/**
@@ -19,7 +29,12 @@ const UserList = ({ users, onRemove }) => {
        * 각각 <User /> 컴포넌트로 만듦, 키값은 id를 사용
        */}
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
