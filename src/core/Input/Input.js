45f3cff054b7db2328bf4dd28c12f34c9f3843d3
lsg001008 <lsg001008@gmail.com>
2022-04-28 11:21:38 +0900

    :lipstick: Add `Input` component w/ SCSS

--- src/core/Input/Input.js
+++ src/core/Input/Input.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styles from './Input.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const Input = ({ name, placeholder }) => {
   return (
-    <div>
+    <div className={cx('Input')}>
       <label>{name}</label>
       <input placeholder={placeholder} />
     </div>
