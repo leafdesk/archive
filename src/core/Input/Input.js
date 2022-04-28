fb3f809bc4d23bb01f776159ca3c5c0ebbac88c1
lsg001008 <lsg001008@gmail.com>
2022-04-28 14:09:37 +0900

    :hammer: Apply state management w/ `useState`

--- src/core/Input/Input.js
+++ src/core/Input/Input.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Input.module.css';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
 const Input = ({ name, type, placeholder }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(() => event.target.value);
+  };
+
   return (
     <div className={cx('Input')}>
       <label>{name}</label>
-      <input type={type} placeholder={placeholder} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
