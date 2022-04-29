4699260d79ea6c0a31b4a48db203b8bf1eca98e2
lsg001008 <lsg001008@gmail.com>
2022-04-29 23:04:48 +0900

    :alembic: Perform experiment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Dropdown from './core/Dropdown/Dropdown.js';
 import Designs from './components/Designs/Designs.js';
 
-const menu = ['Designs'];
+const menuList = ['Designs'];
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         width: 360,
       }}
     >
-      <Dropdown name='MENU' placeholder='Select MENU' optionList={menu} />
+      <Dropdown name="MENU" placeholder="Select MENU" optionList={menuList} />
       <br />
       <div
         style={{
--- src/core/Dropdown/Dropdown.js
+++ src/core/Dropdown/Dropdown.js
@@ -1,31 +1,37 @@
 import React, { useState } from 'react';
 import styles from './Dropdown.module.css';
 import classNames from 'classnames/bind';
-import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+import { MdKeyboardArrowDown } from 'react-icons/md';
 
 const cx = classNames.bind(styles);
 let key = 0;
 
-const Option = ({ option }) => {
-  return <option>{option}</option>;
+const Option = ({ option, value }) => {
+  return <option value={value}>{option}</option>;
 };
 
 const Dropdown = ({ name, placeholder, optionList }) => {
+  const [value, setValue] = useState('init');
+
+  const onChange = (event) => {
+    setValue(() => event.target.value);
+  };
+
   return (
     <div className={cx('Dropdown')}>
       <label>{name}</label>
 
-      <select defaultValue={'init'}>
-        <option value='init' disabled>
+      <select value={value} onChange={onChange}>
+        <option value="init" disabled>
           {placeholder}
         </option>
 
         {optionList.map((option) => (
-          <Option option={option} key={key++} />
+          <Option option={option} value={value} key={key++} />
         ))}
       </select>
 
-      <MdKeyboardArrowDown className={cx('arrow')} size='25' />
+      <MdKeyboardArrowDown className={cx('arrow')} size="25" />
     </div>
   );
 };
