8d529ade4d998805d18950481778cd338b199509
lsg001008 <lsg001008@gmail.com>
2022-05-03 17:02:58 +0900

    :alembic: Perform experiment

--- react-movie-cra/src/App.js
+++ react-movie-cra/src/App.js
@@ -1,7 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function Hello() {
+  function effectFn() {
+    console.log('created');
+  }
+  useEffect(effectFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <h1>Init</h1>
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
