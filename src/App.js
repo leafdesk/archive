b3d7c57d8492ccf8316dd8494de282bfa534709c
lsg001008 <lsg001008@gmail.com>
2022-05-06 13:05:57 +0900

    :hammer: Update app menus

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Dropdown from './core/Dropdown/Dropdown.js';
-import Designs from './components/Designs/Designs.js';
+import InputDesigns from './components/Designs/InputDesigns';
+import ButtonDesigns from './components/Designs/ButtonDesigns.js';
+import Laboratory from './components/Laboratory/Laboratory.js';
 
-const menuList = ['Designs'];
+const menuList = ['Input Designs', 'Button Designs', 'Laboratory'];
 
 function App() {
   const [menu, setMenu] = useState();
@@ -24,6 +26,7 @@ function App() {
         setParentState={onChange}
       />
       <br />
+      {/* hr 대신 아래 div 사용 */}
       <div
         style={{
           height: 1,
@@ -32,7 +35,9 @@ function App() {
       />
       <br />
 
-      {menu === 'Designs' ? <Designs /> : null}
+      {menu === 'Input Designs' ? <InputDesigns /> : null}
+      {menu === 'Button Designs' ? <ButtonDesigns /> : null}
+      {menu === 'Laboratory' ? <Laboratory /> : null}
     </div>
   );
 }
