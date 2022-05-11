3960534b009e82fd923acd648bb9954b975685ad
lsg001008 <lsg001008@gmail.com>
2022-05-11 15:01:53 +0900

    :hammer: Add `Detail` and `Home` components

--- react-movie-cra/src/App.js
+++ react-movie-cra/src/App.js
@@ -1,44 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        'https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year'
-      )
-    ).json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
   return (
-    <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        movies.map((movie) => (
-          <div key={movie.id}>
-            <h3>{movie.title}</h3>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((genre) => (
-                <li key={genre}>{genre}</li>
-              ))}
-            </ul>
-          </div>
-        ))
-      )}
-    </>
+    <Router>
+      <Switch>
+        <Route path='/movie'>
+          <Detail />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
