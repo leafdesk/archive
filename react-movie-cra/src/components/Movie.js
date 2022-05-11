bf3c57999928430a82963536012305ec8e387a9b
lsg001008 <lsg001008@gmail.com>
2022-05-11 15:00:35 +0900

    :hammer: Add `Movie` component

---
+++ react-movie-cra/src/components/Movie.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Movie = ({ coverImg, title, summary, genres }) => {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h3>
+        <Link to='/movie'>{title}</Link>
+      </h3>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
