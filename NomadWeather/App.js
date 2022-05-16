81b13ca3df0666d3e94a814d86ce2e714ddf0fbd
lsg001008 <lsg001008@gmail.com>
2022-05-16 17:50:26 +0900

    :alembic: Perform experiment w/ `Dimensions`, `ScrollView`

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,11 +1,69 @@
-import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
-      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'teal' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        indicatorStyle='white'
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 28,
+    fontWeight: '600',
+  },
+  weather: {
+    backgroundColor: 'lightblue',
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+    fontWeight: '600',
+  },
+  description: {
+    fontSize: 24,
+  },
+});
