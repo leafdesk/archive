4197cfee2e773ac7f7b66708f84e5844f9201f1f
lsg001008 <lsg001008@gmail.com>
2022-05-17 17:57:25 +0900

    :alembic: Perform experiment w/ `expo-location`

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,9 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) setOk(false);
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+
+    console.log(location);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -51,9 +77,7 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: '600',
   },
-  weather: {
-    backgroundColor: 'lightblue',
-  },
+  weather: {},
   day: {
     width: SCREEN_WIDTH,
     alignItems: 'center',
