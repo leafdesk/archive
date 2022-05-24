ee3810a2952dbeed84bd9b2532eecdd31fd17756
lsg001008 <lsg001008@gmail.com>
2022-05-24 10:25:19 +0900

    :hammer: Update Movie.js

--- react-movie-cra/src/components/Movie.js
+++ react-movie-cra/src/components/Movie.js
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const Movie = ({ id, coverImg, title, summary, genres }) => {
   return (
-    <div>
+    <div className={cx('Movie')}>
+      {/* Left Image */}
       <img src={coverImg} alt={title} />
-      <h3>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h3>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+
+      {/* Right Box */}
+      <div className={cx('TextBox')}>
+        <h3>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h3>
+        <ul>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
