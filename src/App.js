4b8602d0078cc0842f8971fc7499aae945158389
lsg001008 <lsg001008@gmail.com>
2022-05-30 12:05:52 +0900

    :hammer: Update App.js w/ `FlexBox`, `Sidebar`, `classNames`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import FlexBox from './core/FlexBox/FlexBox';
+import Sidebar from './components/Sidebar/Sidebar';
 import MobileDesigns from './components/Tabs/MobileDesigns';
+
+import styles from './App.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const App = () => {
   return (
-    <>
-      <MobileDesigns />
-    </>
+    <div className={cx('App')}>
+      <FlexBox>
+        <Sidebar />
+
+        <FlexBox justifyContent='center'>
+          <MobileDesigns />
+        </FlexBox>
+      </FlexBox>
+    </div>
   );
 };
 
