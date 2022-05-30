1576778ddd2828b9085d4e79f10541cfd10770bd
lsg001008 <lsg001008@gmail.com>
2022-05-30 09:58:40 +0900

    :recycle: Move app resources to `MobileDesigns` component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
-import React, { useState } from 'react';
-import Dropdown from './core/Dropdown/Dropdown.js';
-import InputDesigns from './components/Designs/InputDesigns';
-import ButtonDesigns from './components/Designs/ButtonDesigns.js';
-import TodoList from './components/TodoList/TodoList.js';
-import Laboratory from './components/Laboratory/Laboratory.js';
-import CoinTracker from './components/CoinTracker/CoinTracker';
+import MobileDesigns from './components/Tabs/MobileDesigns';
 
-const menuList = [
-  'Input Designs',
-  'Button Designs',
-  'Todo List',
-  'Coin Tracker',
-  'Laboratory',
-];
-
-function App() {
-  const [menu, setMenu] = useState();
-
-  const onChange = (menu) => {
-    setMenu(menu);
-  };
-
+const App = () => {
   return (
-    <div
-      style={{
-        width: 360,
-      }}
-    >
-      <Dropdown
-        label='MENU'
-        placeholder='Select MENU'
-        optionList={menuList}
-        setParentState={onChange}
-      />
-      <br />
-      {/* hr 대신 아래 div 사용 */}
-      <div
-        style={{
-          height: 1,
-          backgroundColor: '#c7cad1',
-        }}
-      />
-      <br />
-
-      {menu === 'Input Designs' ? <InputDesigns /> : null}
-      {menu === 'Button Designs' ? <ButtonDesigns /> : null}
-      {menu === 'Todo List' ? <TodoList /> : null}
-      {menu === 'Laboratory' ? <Laboratory /> : null}
-      {menu === 'Coin Tracker' ? <CoinTracker /> : null}
-    </div>
+    <>
+      <MobileDesigns />
+    </>
   );
-}
+};
 
 export default App;
