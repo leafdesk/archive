4bdde9176a6b197829cd9ca8c051ff84857c2b68
lsg001008 <lsg001008@gmail.com>
2022-06-02 14:35:45 +0900

    :hammer: Add `ContentView` component

---
+++ src/components/ContentView/ContentView.js
@@ -0,0 +1,13 @@
+const ContentView = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default ContentView;
