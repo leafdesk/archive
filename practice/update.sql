0021269fa9b72f74df491868075283440b6980d0
lsg001008 <lsg001008@gmail.com>
2022-06-09 12:08:20 +0900

    :alembic: Perform experiment w/ `UPDATE`

--- practice/update.sql
+++ practice/update.sql
@@ -23,3 +23,23 @@ SET
 WHERE
 	`date` < DATE('2021-01-01') AND num_sequences < 100
 ;
+
+-- id 열 추가 (행 수정 실습을 위해) : INT, [NOT NULL, AUTO_INCREMENT, PRIMARY KEY], FIRST
+-- PRIMARY KEY : 중복되지 않는 고유한 값
+
+ALTER TABLE
+	covid.variants
+ADD
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST
+;
+
+-- 특정 행 수정 : id가 1인 행의 num_sequences와 perc_sequences를 99로 수정
+
+UPDATE
+	covid.variants
+SET
+	num_sequences = 99
+  , perc_sequences = 99
+WHERE
+	id = 1
+;
