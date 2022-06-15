2d8c9915dde4fab28dd96d9f3678da84a7bb7d70
lsg001008 <lsg001008@gmail.com>
2022-06-15 17:12:04 +0900

    :hammer: Add firebase configuration

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDfal1RJyF0yb0XWbzRJ0lJke7R8jT9nVo',
+  authDomain: 'nwitter-7374e.firebaseapp.com',
+  projectId: 'nwitter-7374e',
+  storageBucket: 'nwitter-7374e.appspot.com',
+  messagingSenderId: '183510226123',
+  appId: '1:183510226123:web:798230458c41ef7513374e',
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
