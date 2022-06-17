bcfbe0987000e163b22a601cbc465b4d323608f1
lsg001008 <lsg001008@gmail.com>
2022-06-17 17:58:42 +0900

    :hammer: Update App.js

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,5 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import AppRouter from 'components/Router';
+import app from 'fbase';
+import { getAuth } from 'firebase/auth';
+
 function App() {
-  return <></>;
+  const auth = getAuth();
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
