f0379936620b931f22cecbde494d8838bfa928a7
lsg001008 <lsg001008@gmail.com>
2022-06-21 16:58:25 +0900

    :hammer: Update `Navbar` component

--- next-intro/components/Navbar.js
+++ next-intro/components/Navbar.js
@@ -1,30 +1,64 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  // 현재 주소창의 루트를 알 수 있음
   const router = useRouter();
 
   return (
-    <nav>
-      <Link href='/'>
-        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-      </Link>
+    <>
+      <div>
+        <img src='/vercel.svg' />
+      </div>
 
-      <Link href='/about'>
-        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-      </Link>
+      <nav>
+        <Link href='/'>
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
 
-      <style jsx global>{`
-        a {
-          text-decoration: none;
-          color: white;
+        <Link href='/about'>
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </nav>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          align-items: center;
+          justify-content: center;
+          padding-top: 10px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.1) 0px 16px 24px -2px;
         }
+
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 24px 0 0;
+        }
+
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+
         .active {
-          color: blue;
+          color: tomato;
         }
       `}</style>
-    </nav>
+    </>
   );
 };
 
