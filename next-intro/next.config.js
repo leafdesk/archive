f322acb97c65d896b6c4ae9625cf5abd1e46ef62
lsg001008 <lsg001008@gmail.com>
2022-06-23 13:09:22 +0900

    :hammer: Update next.config.js

--- next-intro/next.config.js
+++ next-intro/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = 'c36f93ac62d42e110425fd7987353e63';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: '/old/:path',
+        destination: '/new/:path',
+        permanent: false,
+      },
+    ];
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
