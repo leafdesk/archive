1b076b76776f7bbc18d5fb8f39b46a853e983b90
lsg001008 <lsg001008@gmail.com>
2022-06-27 13:53:52 +0900

    :recycle: Refactor Department.js w/ SCSS and `classNames`

--- src/components/Department/Department.js
+++ src/components/Department/Department.js
@@ -9,7 +9,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import bwmLogo from '../../../public/images/bwm_logo.png';
 
-import styles from './Department.module.css';
+import styles from './Department.module.scss';
 import classNames from 'classnames/bind';
 
 const cn = classNames.bind(styles);
@@ -29,10 +29,10 @@ const Department = () => {
           <SwiperSlide className={cn('SwiperSlide')}>
             <Link href='youtube://channel/UCkrWb-HCk3fA7szpbmLHTmw'>
               <a>
-                <div className='img'>
+                <div className={cn('img')}>
                   <Image src={bwmLogo} placeholder='blur' quality={100} />
                 </div>
-                <div className='txt'>청년부</div>
+                <div className={cn('txt')}>청년부</div>
               </a>
             </Link>
           </SwiperSlide>
@@ -40,10 +40,10 @@ const Department = () => {
           <SwiperSlide className={cn('SwiperSlide')}>
             <Link href='youtube://channel/UCW6bF9L0ZK__Tlwl19B0FYQ'>
               <a>
-                <div className='img'>
+                <div className={cn('img')}>
                   <img src='../icons/thumb_cba.svg' alt='대학부' />
                 </div>
-                <div className='txt'>대학부</div>
+                <div className={cn('txt')}>대학부</div>
               </a>
             </Link>
           </SwiperSlide>
@@ -51,10 +51,10 @@ const Department = () => {
           <SwiperSlide className={cn('SwiperSlide')}>
             <Link href='youtube://channel/UCcD3GeLh6aBwBN_A5yr4pEg'>
               <a>
-                <div className='img'>
+                <div className={cn('img')}>
                   <img src='../icons/thumb_high.svg' alt='고등부' />
                 </div>
-                <div className='txt'>고등부</div>
+                <div className={cn('txt')}>고등부</div>
               </a>
             </Link>
           </SwiperSlide>
@@ -62,10 +62,10 @@ const Department = () => {
           <SwiperSlide className={cn('SwiperSlide')}>
             <Link href='youtube://channel/UCDzjhPXk9IypRuCopoFDvlg'>
               <a>
-                <div className='img'>
+                <div className={cn('img')}>
                   <img src='../icons/thumb_secondary.svg' alt='중등부' />
                 </div>
-                <div className='txt'>중등부</div>
+                <div className={cn('txt')}>중등부</div>
               </a>
             </Link>
           </SwiperSlide>
@@ -73,10 +73,10 @@ const Department = () => {
           <SwiperSlide className={cn('SwiperSlide')}>
             <Link href='youtube://channel/UCVZgyTaNK1q-CKM481MO35A'>
               <a>
-                <div className='img'>
+                <div className={cn('img')}>
                   <img src='../icons/thumb_elementary.svg' alt='유치부' />
                 </div>
-                <div className='txt'>어린이부</div>
+                <div className={cn('txt')}>어린이부</div>
               </a>
             </Link>
           </SwiperSlide>
