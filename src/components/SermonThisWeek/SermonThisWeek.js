b1b2075709f0d1dcbb9fec26eeb3818c75f19035
lsg001008 <lsg001008@gmail.com>
2022-07-12 17:59:32 +0900

    :hammer: Update `SermonThisWeek` component

--- src/components/SermonThisWeek/SermonThisWeek.js
+++ src/components/SermonThisWeek/SermonThisWeek.js
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router';
 import YouTube from 'react-youtube';
 import Loading from '../Loading';
 import Share from '../Share';
-
 import styles from './SermonThisWeek.module.scss';
 import classNames from 'classnames/bind';
 
@@ -20,15 +19,17 @@ const SermonThisWeek = ({ isLoading, liveDatas }) => {
   return (
     <>
       {isLoading === false ? (
-        <div className='section pt0'>
-          <div className={cn('movie_wrap')}>
+        <section className='section pt0'>
+          <div className={cn('Movie')}>
             <YouTube
               videoId={liveDatas.videoId}
               opts={opts}
               containerClassName='iframe_wrap'
             />
 
+            {/* 영상 정보(Info) */}
             <div className={cn('info')}>
+              {/* 공유 버튼 */}
               <Share
                 className={cn('btn_share')}
                 title={liveDatas.title}
@@ -36,8 +37,9 @@ const SermonThisWeek = ({ isLoading, liveDatas }) => {
                 vid={liveDatas.videoId}
               />
 
+              {/* 영상 제목 */}
               <div
-                className='tit pr25'
+                className={cn('tit', 'pr25')}
                 onClick={() => {
                   router.push(
                     `/sermondetail?vid=${liveDatas.videoId}&vtit=${liveDatas.title}&vdate=${liveDatas.publishedAt}`,
@@ -48,15 +50,16 @@ const SermonThisWeek = ({ isLoading, liveDatas }) => {
                 <a href='#'>{liveDatas.title}</a>
               </div>
 
+              {/* 영상 날짜 및 설교자 */}
               <div className={cn('date')}>{liveDatas.publishedAt}</div>
               <div className={cn('preacher')}>설교: 김성현 목사</div>
             </div>
             {/* end of info */}
           </div>
-          {/* end of movie_wrap */}
-        </div>
+          {/* end of Movie */}
+        </section>
       ) : (
-        <div className={cn('loading_box')}>
+        <div className={cn('Loading')}>
           <Loading />
         </div>
       )}
