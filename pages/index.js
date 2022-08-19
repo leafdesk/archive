ea2ea677a7e31867a58ae4e0901700ce1a3cc2a2
lsg001008 <lsg001008@gmail.com>
2022-08-19 13:22:18 +0900

    :alembic: Perform experiment

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import MemberAdditionForm from '../src/components/MemberAdditionForm/MemberAdditionForm';
+import Checkout from '../src/components/Checkout/Checkout';
 
 const Home = () => {
   return (
     <>
       <MemberAdditionForm />
+      {/* <Checkout /> */}
     </>
   );
 };
