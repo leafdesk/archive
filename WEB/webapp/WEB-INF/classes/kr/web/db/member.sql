e8ee7fc78feb60947c0d005bc3990da726fd822f
lsg001008 <lsg001008@gmail.com>
2022-08-27 14:16:40 +0900

    :hammer: Update sql file

--- WEB/webapp/WEB-INF/classes/kr/web/db/member.sql
+++ WEB/webapp/WEB-INF/classes/kr/web/db/member.sql
@@ -7,3 +7,12 @@ create table member(
 	email varchar(30) not null,
 	phone varchar(30) not null
 )
+
+select * from member;
+
+insert into member(id, pass, name, age, email, phone)
+values('admin', 'admin', '관리자', 40, 'bit@naver.com', '010-1111-1111');
+
+update member set age=45, phone='010-1111-0000' where id='admin';
+
+delete from member where id='admin';
