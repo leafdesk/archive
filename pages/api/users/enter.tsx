7e7c394e5ff481d0bb6ad9afceaea9e69c46bf6a
lsg001008 <lsg001008@gmail.com>
2022-08-30 12:04:32 +0900

    :hammer: Update enter.tsx

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,21 +1,34 @@
+import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../../libs/client/client';
-import withHandler from '../../../libs/server/withHandler';
+import withHandler, { ResponseType } from '../../../libs/server/withHandler';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) => {
   const { phoneNumber, email } = req.body;
   console.log('클라이언트에서 다음을 요청함: ', req.body);
 
-  const payload = email ? { email } : { phoneNumber: +phoneNumber };
+  const member = phoneNumber
+    ? { phoneNumber: +phoneNumber }
+    : email
+    ? { email }
+    : null;
+  if (!member) return res.status(400).json({ ok: false });
+
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
   // const member = await client.member.upsert({
   //   where: {
-  //     ...payload,
+  //     ...member,
   //   },
   //   create: {
   //     name: 'Anonymous',
   //     sex: 'unknown',
   //     department: 'unclassified',
-  //     ...payload,
+  //     ...member,
   //   },
   //   update: {},
   // });
@@ -24,17 +37,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const token = await client.token.create({
     data: {
-      payload: '1234',
+      payload,
       member: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...member,
           },
           create: {
-            name: 'Anonymous',
-            sex: 'unknown',
-            department: 'unclassified',
-            ...payload,
+            name: '(알 수 없음)',
+            sex: '(알 수 없음)',
+            department: '(알 수 없음)',
+            ...member,
           },
         },
       },
@@ -42,6 +55,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   });
 
   console.log(token);
+
+  if (phoneNumber) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `[인증번호: ${payload}] 사용자 본인 인증번호를 입력해주세요. (성락회원관리시스템)`,
+    });
+    console.log(message);
+  }
 
   // if (email) {
   //   member = await client.member.findUnique({
@@ -97,7 +119,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   //   console.log(member);
   // }
 
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 };
 
 export default withHandler('POST', handler);
