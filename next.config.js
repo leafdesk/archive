8acb24fb356c7c76685749b96178feb16852905a
lsg001008 <lsg001008@gmail.com>
2022-09-02 13:09:19 +0900

    :wrench: Update configurations

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
