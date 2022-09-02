24a2cda325026d39ccdc34c23e8b9d67308fdc54
lsg001008 <lsg001008@gmail.com>
2022-09-02 17:13:09 +0900

    :hammer: Update withHandler.ts

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -9,7 +9,10 @@ const withHandler = (
   method: 'GET' | 'POST' | 'DELETE',
   fn: (req: NextApiRequest, res: NextApiResponse) => void
 ) => {
-  return async function name(req: NextApiRequest, res: NextApiResponse) {
+  return async function name(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ): Promise<any> {
     if (req.method !== method) {
       return res.status(405).end();
     }
