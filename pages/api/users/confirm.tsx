352bd9b012ec9e97dd1b407debdba1cc78567589
lsg001008 <lsg001008@gmail.com>
2022-09-02 16:37:37 +0900

    :hammer: Add confirm api

---
+++ pages/api/users/confirm.tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '../../../libs/client/client';
+import withHandler, { ResponseType } from '../../../libs/server/withHandler';
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) => {
+  const { token } = req.body;
+  console.log(token);
+  res.status(200).end();
+};
+
+export default withHandler('POST', handler);
