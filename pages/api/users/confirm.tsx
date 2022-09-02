71aedb1dc8ebf6167d7cbc7a76fc8bb23a3f413b
lsg001008 <lsg001008@gmail.com>
2022-09-02 17:13:23 +0900

    :hammer: Update confirm.tsx

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '../../../libs/client/client';
 import withHandler, { ResponseType } from '../../../libs/server/withHandler';
+import { withIronSessionApiRoute } from 'iron-session/next';
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    // include: { member: true },
+  });
+  if (!exists) res.status(404).end();
+  req.session.member = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 };
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'session',
+  password: 'zujYsTCL727AApTDvBYwLAEJEN6GB5Bg',
+});
