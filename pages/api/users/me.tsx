60b3f492e59ae1c1b454acbefde4a34d12bcc952
lsg001008 <lsg001008@gmail.com>
2022-09-07 16:27:18 +0900

    :hammer: Update me.tsx

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -3,15 +3,6 @@ import client from '@libs/client/client';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { withApiSession } from '@libs/server/withSession';
 
-// req.session에 member가 있다는 것을 알려주기 위함.
-declare module 'iron-session' {
-  interface IronSessionData {
-    member?: {
-      id: number;
-    };
-  }
-}
-
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
