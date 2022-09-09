98fcc3d0c7bdec609754977dd04f85cc672a2b2b
lsg001008 <lsg001008@gmail.com>
2022-09-09 16:04:02 +0900

    :wrench: Update next configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
