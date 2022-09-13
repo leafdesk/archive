b5f89f81b5cdc264b1b3dc8a3e4f816fe28b82ac
lsg001008 <lsg001008@gmail.com>
2022-09-13 15:15:07 +0900

    :hammer: Add `isPrivate` property

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,10 +5,13 @@ export interface ResponseType {
   [key: string]: any;
 }
 
-const withHandler = (
-  method: 'GET' | 'POST' | 'DELETE',
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) => {
+interface ConfigType {
+  method: 'GET' | 'POST' | 'DELETE';
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+}
+
+const withHandler = ({ method, isPrivate = true, handler }: ConfigType) => {
   return async function name(
     req: NextApiRequest,
     res: NextApiResponse
@@ -16,8 +19,13 @@ const withHandler = (
     if (req.method !== method) {
       return res.status(405).end();
     }
+
+    if (isPrivate && !req.session.member) {
+      return res.status(401).json({ ok: false, error: '로그인 해주세요.' });
+    }
+
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error });
