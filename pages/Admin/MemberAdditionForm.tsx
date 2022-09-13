a7419bd03b77537462a7415c3b0fbff5cfbadfc4
lsg001008 <lsg001008@gmail.com>
2022-09-13 17:24:25 +0900

    :hammer: Add `MemberAdditionForm` page

---
+++ pages/Admin/MemberAdditionForm.tsx
@@ -0,0 +1,7 @@
+import MemberAdditionForm from '../../src/components/MemberAdditionForm/MemberAdditionForm';
+
+const MemberAdditionFormPage = () => {
+  return <MemberAdditionForm />;
+};
+
+export default MemberAdditionFormPage;
