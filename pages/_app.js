41126513df8149b1cc0f82939830f839c1e76037
lsg001008 <lsg001008@gmail.com>
2022-10-13 18:04:40 +0900

    :hammer: Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/reset.css';
 import '../styles/globals.css';
 import Header from '../src/components/Header/Header';
+import useUser from '@libs/client/useUser';
 
 const MyApp = ({ Component, pageProps }) => {
+  const user = useUser();
+
   return (
     <>
       <Header />
