22a24e64a4592f7500c6701cc783c72a4c41e949
lsg001008 <lsg001008@gmail.com>
2022-10-18 18:17:34 +0900

    :hammer: Update `MemberInquiry` component

--- src/components/MemberInquiry/index.js
+++ src/components/MemberInquiry/index.js
@@ -1,6 +1,8 @@
 import useMutation from '@libs/client/useMutation';
 import { useForm } from 'react-hook-form';
-import { Button, Container, TextField } from '@mui/material';
+import { Button, Container, TextField, Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import useSWR from 'swr';
 
 const MemberInquiry = () => {
   const {
@@ -10,13 +12,37 @@ const MemberInquiry = () => {
     formState: { errors },
   } = useForm();
 
-  const [showMember, { loading, data, error }] = useMutation(
-    '../api/admin/showMember'
-  );
+  const { data } = useSWR('/api/admin/members');
+  console.log(data);
 
-  const onValid = (searchData) => {
-    showMember(searchData);
-  };
+  const columns = [
+    /**
+     * id, createdAt, updatedAt, name, gender, department, address,
+     * school, major, phoneNumber, email, tokens, authority
+     */
+    { field: 'id', headerName: 'ID' },
+    { field: 'createdAt', headerName: 'createdAt' },
+    { field: 'updatedAt', headerName: 'updatedAt' },
+    { field: 'name', headerName: 'name' },
+    { field: 'gender', headerName: 'gender' },
+    { field: 'department', headerName: 'department' },
+    { field: 'address', headerName: 'address' },
+    { field: 'school', headerName: 'school' },
+    { field: 'major', headerName: 'major' },
+    { field: 'phoneNumber', headerName: 'phoneNumber' },
+    { field: 'email', headerName: 'email' },
+    { field: 'tokens', headerName: 'tokens' },
+    { field: 'authority', headerName: 'authority' },
+  ];
+
+  /**
+   * await 안해서 undefined 일때.. Array.length 를 찾을 수 없다고 나옴
+   */
+  const rows = data?.members;
+  // console.log('rows:', rows);
+  // console.log(rows?.length);
+
+  const onValid = (searchData) => {};
 
   const onInvalid = () => {};
 
@@ -24,19 +50,18 @@ const MemberInquiry = () => {
     <>
       <Container>
         <form onSubmit={handleSubmit(onValid, onInvalid)}>
-          <TextField
-            label='이름'
-            type='text'
-            error={errors.name != null}
-            // helperText={errors.name?.message}
-            required
-            {...register('name', {
-              required: '이름을 입력하세요.',
-            })}
-          />
+          <TextField label='이름' type='text' />
 
           <Button type='submit'>조회(검색)</Button>
         </form>
+
+        <Box>
+          <div style={{ display: 'flex', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+              <DataGrid columns={columns} rows={rows} />
+            </div>
+          </div>
+        </Box>
       </Container>
     </>
   );
