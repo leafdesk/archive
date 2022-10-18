6ca541f6fa8249d8f0afb63a64f9b393216842de
lsg001008 <lsg001008@gmail.com>
2022-10-18 18:17:17 +0900

    :hammer: Update withHandler.ts

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,18 +5,20 @@ export interface ResponseType {
   [key: string]: any;
 }
 
+type method = 'GET' | 'POST' | 'DELETE';
+
 interface ConfigType {
-  method: 'GET' | 'POST' | 'DELETE';
+  methods: method[];
   handler: (req: NextApiRequest, res: NextApiResponse) => void;
   isPrivate?: boolean;
 }
 
-const withHandler = ({ method, isPrivate = true, handler }: ConfigType) => {
+const withHandler = ({ methods, isPrivate = true, handler }: ConfigType) => {
   return async function name(
     req: NextApiRequest,
     res: NextApiResponse
   ): Promise<any> {
-    if (req.method !== method) {
+    if (req.method && !methods.includes(req.method as any)) {
       return res.status(405).end();
     }
 
