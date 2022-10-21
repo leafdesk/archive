9b2c2060f069bcb7cdb1d79dc75854d8d6ee4571
lsg001008 <lsg001008@gmail.com>
2022-10-21 17:58:52 +0900

    :alembic: 디자인 시스템 구상 중

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import Top from '../components/templates/Top/TopV1';
+import Card from '../components/templates/Card/CardV1';
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <Top title='주일 설교' />
+      <Card
+        thumbnail='https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/29daac0c-04de-417c-565c-39606d361e00/public'
+        title='예수는 부활과 생명이시다(Jesus is the Resurrection and the life) 요한복음 11:17~30'
+        date='2022.10.16'
+        endOfContainer={true}
+      />
+
+      <Top title='주중 콘텐츠' />
+    </ScrollView>
   );
 }
