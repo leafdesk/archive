4d5efd89ef7beb2829da3e5a1395c6482594b3c9
leafdesk <lsg001008@gmail.com>
2024-02-19 11:18:16 +0900

    :hammer: 스크립트 추가 - navigation component 생성. - root layout에 app navigation menu 추가.

--- packages/next/src/app/layout.tsx
+++ packages/next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Inter as FontSans } from 'next/font/google'
 
 import { cn } from '@lab/sdk/utils/shadcn'
+import Navigation from './navigation'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
@@ -16,11 +17,16 @@ const RootLayout = ({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        // Inter 폰트 설정.
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
           fontSans.variable,
         )}
       >
+        {/* 네비게이션 */}
+        <Navigation />
+
+        {/* 페이지 내용 */}
         {children}
       </body>
     </html>
