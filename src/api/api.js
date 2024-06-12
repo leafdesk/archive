a715d9845212885985ff20a76ed96913121a0e7e
jjonyo <jonyo.park@gmail.com>
2024-06-12 18:11:32 +0900

    feat : query string 방식 변경

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import AX from '@/api/apiConfig'
 
 export const API = {
   join: (data) => AX.post(`/event-api/event/join`, data).then((res) => res),
-  validateNickName: (nickname) => AX.get(`/event-api/event/validate-nickname`, { params: nickname }).then((res) => res),
+  validateNickName: (nickname) => AX.get(`/event-api/event/validate-nickname?nickname=${nickname}`).then((res) => res),
 }
 
