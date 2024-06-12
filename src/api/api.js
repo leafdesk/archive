70f3e2c098bc700030f1735a3ff40397383fdfd8
Yoon-HP <dkwjt001@korea.ac.kr>
2024-06-12 22:03:02 +0900

    chore: API 경로 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import AX from '@/api/apiConfig'
 
 export const API = {
   join: (data) => AX.post(`/event-api/event/join`, data).then((res) => res),
+  joinTest: () => AX.get(`/event-api/event/test`).then((res) => res),
   validateNickName: (nickname) => AX.get(`/event-api/event/validate-nickname?nickname=${nickname}`).then((res) => res),
+  fetchTeamList: () => AX.get(`/event-api/event/teams`).then((res) => res),
 }
 
