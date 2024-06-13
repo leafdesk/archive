e614cfacbe123980b1c65aa6dd96b568b227886d
leafdesk <lsg001008@gmail.com>
2024-06-13 16:32:43 +0900

    update: 홈 페이지에서 이벤트 데이터 fetch 로직을 커스텀 훅으로 분리

--- src/app/(event)/home/page.jsx
+++ src/app/(event)/home/page.jsx
@@ -1,64 +1,70 @@
-'use client' // Next.js v14 에서 사용자 상호 작용을 위해 최상단 고정 필수.
+'use client'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import useLocalStorage from '@/hooks/useLocalStorage'
-import localStorageKey from '@/constants/localStorageKey'
-import { API } from '@/api/api'
+import useEventData from '@/hooks/useEventData'
 
-const EventHome = () => {
-  const router = useRouter()
-  const { getLocalStorageData } = useLocalStorage()
-  const [eventData, setEventData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
+/**
+ * 팀 페이지. (이벤트 홈)
+ */
+const EventHomePage = () => {
+  const { data: eventData, loading, error } = useEventData()
+  // console.log(eventData, loading, error)
 
-  useEffect(() => {
-    const token = getLocalStorageData(localStorageKey.EVENT_TOKEN)
-    if (!token) {
-      router.push('/join')
-      return
-    }
-
-    const fetchEventData = async () => {
-      try {
-        const response = await API.joinTest()
-        if (response.status === 200) {
-          setEventData(response.data)
-        } else {
-          setError('Failed to fetch event data.')
-        }
-      } catch (error) {
-        console.error('Error fetching event data:', error)
-        setError('Failed to fetch event data.')
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchEventData()
-  }, [router, getLocalStorageData])
-
+  /**
+   * 이벤트 데이터 로딩 중 UI.
+   */
   if (loading) {
     return <div>Loading...</div>
   }
 
+  /**
+   * 이벤트 데이터 에러 발생 시 UI.
+   */
   if (error) {
     return <div>{error}</div>
   }
 
+  /**
+   * 이벤트 데이터 로딩 완료 시 UI.
+   */
   return (
-    <div>
-      <h1>Event Home</h1>
+    <>
       {eventData ? (
-        <div>
-          <h2>Event Data:</h2>
-          <pre>{JSON.stringify(eventData, null, 2)}</pre>
-        </div>
+        <>
+          {/* 홈 상단 */}
+          <div>
+            <button>
+              미션방법
+              <svg />
+            </button>
+            <button>
+              전체 미션보드
+              <svg />
+            </button>
+          </div>
+
+          {/* 팀, 닉네임, 소속 기관 */}
+          <div>
+            <div>
+              <h2>1팀</h2>
+              <h3>{eventData}님</h3>
+            </div>
+            <span>가평, 김포, 부천, 이천, 강남, 중등부, 대학부(온,로뎀)</span>
+          </div>
+
+          {/* 팀 미션보드 */}
+          <div>
+            {/* 이미지 div */}
+            <div />
+          </div>
+        </>
       ) : (
+        /**
+         * 이벤트 데이터가 없을 시 UI.
+         */
         <div>No event data available.</div>
       )}
-    </div>
+    </>
   )
 }
 
-export default EventHome
+export default EventHomePage
