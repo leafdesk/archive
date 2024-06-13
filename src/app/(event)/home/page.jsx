90f26cdc27ee7f7432ed7d70a8bd0e6dd52ea819
leafdesk <lsg001008@gmail.com>
2024-06-13 17:36:10 +0900

    design: 홈 페이지 및 미션 카드 퍼블리싱

--- src/app/(event)/home/page.jsx
+++ src/app/(event)/home/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import MissionCard from '@/components/mission-card'
 import useEventData from '@/hooks/useEventData'
 
 /**
@@ -29,34 +30,53 @@ const EventHomePage = () => {
   return (
     <>
       {eventData ? (
-        <>
+        <div className="bg-[#F7F7F7]">
           {/* 홈 상단 */}
-          <div>
-            <button>
+          <div className="h-[54px] px-4 mb-9 flex items-center justify-end">
+            <button className="font-normal text-[#666] text-base w-fit bg-white rounded-full h-[38px] flex items-center justify-center px-[14px]">
               미션방법
-              <svg />
             </button>
-            <button>
+            <button className="font-normal text-[#666] text-base w-fit bg-white rounded-full h-[38px] flex items-center justify-center px-[14px] ml-2">
               전체 미션보드
-              <svg />
             </button>
           </div>
 
           {/* 팀, 닉네임, 소속 기관 */}
-          <div>
-            <div>
-              <h2>1팀</h2>
-              <h3>{eventData}님</h3>
+          <div className="px-4 pb-6">
+            <div className="flex items-end mb-1">
+              <h2 className="font-medium text-2xl text-[#222] mr-1">1팀</h2>
+              <h3 className="font-medium text-base text-[#222]">
+                {eventData}님
+              </h3>
             </div>
-            <span>가평, 김포, 부천, 이천, 강남, 중등부, 대학부(온,로뎀)</span>
+            <span className="text-sm font-normal text-[#888]">
+              가평, 김포, 부천, 이천, 강남, 중등부, 대학부(온,로뎀)
+            </span>
           </div>
 
           {/* 팀 미션보드 */}
-          <div>
+          <div className="px-4 pb-6">
             {/* 이미지 div */}
-            <div />
+            {/* 🚨 TODO: 실제로는 고정 height가 아니라 w-fit에 맞는 height 비율로 생각해야 함. */}
+            <div className="h-[268px] bg-blue-600" />
           </div>
-        </>
+
+          {/* 미션 카드 리스트 */}
+          <div className="px-4 pb-6 grid gap-4">
+            <MissionCard
+              number={1}
+              title="감독님을 향한 사랑의 편지 작성하기"
+              colorIndex={0}
+              isPendingApproval={true}
+            />
+            <MissionCard
+              number={2}
+              title="SUM WORSHIP 좋아요, 댓글 남기기, 구독, 알림설정 하기"
+              colorIndex={1}
+              isPendingApproval={false}
+            />
+          </div>
+        </div>
       ) : (
         /**
          * 이벤트 데이터가 없을 시 UI.
--- src/components/mission-card.jsx
+++ src/components/mission-card.jsx
@@ -3,29 +3,67 @@
 /**
  * 미션 카드.
  */
-const MissionCard = ({ number, title, rate }) => {
+const MissionCard = ({
+  number,
+  title,
+  rate,
+  colorIndex,
+  isPendingApproval,
+}) => {
+  const colors = [
+    { background: 'bg-[#E0E6F6]', text: 'text-[#446CC1]' },
+    { background: 'bg-[#ECF8F4]', text: 'text-[#107054]' },
+  ]
+  // const colorIndex = 0
+  const backgroundColor = colors[colorIndex]?.background
+  const textColor = colors[colorIndex]?.text
+
+  // 승인 대기 상태. (true: 승인 대기 중, false: 인증하기)
+  // const isPendingApproval = true
+
   return (
-    <div>
-      {/* 왼쪽: 미션 번호 */}
-      <span>미션 1</span>
+    <div className="flex items-center justify-between w-full rounded-lg overflow-hidden border border-[#EAEAEA]">
+      {/* 카드의 왼쪽: 미션 번호 */}
+      <div
+        className={`${backgroundColor} w-[55px] min-w-[55px] h-full flex items-center justify-center`}
+      >
+        <span className={`${textColor} font-medium text-base`}>
+          미션 {number}
+        </span>
+      </div>
 
-      {/* 오른쪽 */}
-      <div>
+      {/* 카드의 오른쪽 */}
+      <div className="pt-4 px-4 w-full h-full pb-2.5 bg-white flex flex-col">
         {/* 미션 제목 */}
-        <strong>감독님을 향한 사랑의 편지 작성하기</strong>
+        <strong className="block font-medium text-base text-[#222]">
+          {title}
+        </strong>
 
-        {/* 참여율 상태 바 */}
+        {/* 참여율 상태 바 - Linear Gradient #6679FF 100% ~ #EBABDC 100% */}
+        <div className="w-full h-[6px] bg-[#6679FF] block my-2" />
 
-        {/* 격려 텍스트 */}
-        <span>조금 더 힘내세요!</span>
+        <div className="flex items-start justify-between mb-4">
+          {/* 격려 텍스트 */}
+          <span className="font-medium text-xs text-[#777DF0]">
+            조금 더 힘내세요!
+          </span>
 
-        {/* 참여율 퍼센트 텍스트 */}
-        <span>참여율 84%</span>
+          {/* 참여율 퍼센트 텍스트 */}
+          <span className="font-normal text-xs text-[#666]">참여율 84%</span>
+        </div>
 
         {/* 하단 버튼 */}
-        <div>
-          <button>상세보기</button>
-          <button>승인 대기 중 </button>
+        <div className="mt-auto flex items-center justify-end">
+          <button className="font-normal text-sm text-[#888]">상세보기</button>
+          {isPendingApproval ? (
+            <button className="block w-[110px] rounded-md border border-[#AAA] h-[34px] font-medium text-base text-[#AAA] ml-5">
+              승인 대기 중
+            </button>
+          ) : (
+            <button className="block w-[110px] rounded-md h-[34px] font-medium text-base ml-5 bg-[#19CD72] text-white">
+              인증하기
+            </button>
+          )}
         </div>
       </div>
     </div>
